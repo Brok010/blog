d18736cb507a0dd1d310fc0fc281c9e84c97f6f7
Borek Vodrážka <brokvo@gmail.com>
2023-12-11 13:28:34 +0100

    thai 1st post

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
     
 <?php include('includes/header.php'); ?>
-<?php include('includes/navbar.php'); ?>
+
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3">
+    <div class="container">
+        <a href="#" class="navbar-brand">Brok's blog</a>
+        <button class="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navmenu"
+            >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item mx-2">
+                    <a href="travel.php" class="nav-link">Travel</a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a href="sport.php" class="nav-link">Sport</a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a href="#about-me" class="nav-link">About me</a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a href="#contact" class="nav-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+    
     <body>
         <!-- showcase -->
         <section class="bg-white text-dark p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
